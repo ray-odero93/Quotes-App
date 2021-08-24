d6ce37176d8220ff8825d666362cebbef7e22af6
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-08-24 16:03:21 +0300

    Declare quotes and list property values in the quote.component.ts file.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote'
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes: Quote[] = [
+    {
+      userName: "Ray",
+      author: "Mahatma Gandhi",
+      saying: "Be the change you wish to see in the world.",
+      upvotes: 0,
+      downvotes: 0,
+      postDate: new Date
+    },
+    {
+      userName: "Purity",
+      author: "Oscar Wilde",
+      saying: "Be yourself, everyone else is taken.",
+      upvotes: 0,
+      downvotes: 0,
+      postDate:new Date
+    }
+  ]
 
   constructor() { }
 
