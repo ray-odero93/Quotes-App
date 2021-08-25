de57e4dda703b1b1cdfc1b3314bb323d4c2638d1
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-08-25 14:04:17 +0300

    Add an input data binding for quote-detail component to parent component, quote component.

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quote-details',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+  @Input()
+  quote!: Quote;
 
   constructor() { }
 
