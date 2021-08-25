7cea9d0eb5fe684808ac9e032126dac5050d60ed
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-08-25 14:02:33 +0300

    Create a constructor for quote class and declare showDescription variable.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,15 +1,12 @@
 export class Quote {
-  public upvotes!: number
-  public downvotes!: number
-  public datePosted!: Date
 
+  public showDescription?: boolean;
   constructor(
     public name: string,
+    public saying: string,
     public author: string,
-    public words:string
+    public description: number[]
   ) {
-    this.upvotes = 0;
-    this.downvotes = 0;
-    this.datePosted = new Date;
+    this.showDescription = false;
   }
 }
